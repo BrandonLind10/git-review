1718b7386ebc2d96ed94476d13d9aa976de1c65e
BrandonLind10 <45861283+BrandonLind10@users.noreply.github.com>
2019-04-09 23:27:08 -0700

    Update cat.js
    added giveBirth method and static getRandomAge methd

--- cat.js
+++ cat.js
@@ -1,12 +1,34 @@
 class Cat{
-    constructor(name,age,sex){
+    constructor(name,age=.1,sex){
         this.name = name;
         this.age = age;
     }
     
     growUp(){
-        this.age=+1;
+        if(this.age < 1){
+             this.age+= .1;
+         }
+        else {
+           this.age+= 1;
+        }
     }
-    
-
-}
+    static getRandomSex(){
+            let randomNumber = Math.floor(Math.random() * 2 ) + 1;
+            if(randomNumber === 1){
+                return "female";
+           }
+            else {
+                return "male";
+            }
+        }
+        
+        
+    giveBirth(){
+        if(this.age > .4){
+            console.log("sorry " + this.name + " is " + " too young");
+        }
+        else{
+            let kitten = new Cat(null, .1 , Cat.getRandomSex());
+            return kitten;
+            }
+    }
